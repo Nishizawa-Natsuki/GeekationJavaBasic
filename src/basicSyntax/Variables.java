9ac6d07d9f21148f45fb6014031a5fdd6c911922
Nishizawa-Natsuki <n.nishizawa.cingroup2012@gmail.com>
2024-07-08 21:35:47 +0900

    変数の代入と実装

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,16 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int num1 = 9 ;
+		double num2 = 9.9 ;
+		String like = "私の好きな";
+		String last = "です。";
+		System.out.println(num1);
+		System.out.println(num2);
+		
+		System.out.println(like + "整数は" + num1 + last);
+		System.out.println(like + "小数は" + num2 + last);
+	}
+}
