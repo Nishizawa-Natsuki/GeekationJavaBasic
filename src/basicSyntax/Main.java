4235c1f4636e88c85cdad72d59ecae4b67e2060d
Nishizawa-Natsuki <n.nishizawa.cingroup2012@gmail.com>
2024-07-25 23:11:38 +0900

    Main.javaの修正

--- src/basicSyntax/Main.java
+++ src/basicSyntax/Main.java
@@ -6,9 +6,6 @@ public class Main {
 		System.out.println("こんにちは! 私はGeek君でごわす。");
 		System.out.println("年は" + age + "でごわす。");
 		System.out.println("身長は" + height + "でごわす。");
-		
-		
-		
 	}
 	
 	public void specialSkill(int num1, int num2) {
@@ -16,14 +13,17 @@ public class Main {
 			System.out.println("num1とnum2は0より大きい値にしてください。");
 			return;
 		}
+		
 		if (num1 >= num2) {
 			System.out.println("num2はnum1より大きい値にしてください。");
 			return;
 		}
+		
 		if (num1 > 300 || num2 > 300) {
 			System.out.println("num1とnum2は300以下にしてください。");
 			return;
 		}
+		
 		for (int i = num1; i <= num2; i++) {
 			if (i % 3 == 0 && i % 5 == 0) {
 				System.out.println(i + "は3の倍数かつ5の倍数です。");
@@ -37,10 +37,8 @@ public class Main {
 			
 			if (i % 5 == 0) {
 				System.out.println(i + "は5の倍数です。");
-				continue; // 次の数値へ
+				continue;
 			}
 		}
 	}
-	
-	
 }
